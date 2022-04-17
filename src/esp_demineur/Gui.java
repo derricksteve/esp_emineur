94f161986c63db71ed00853c6012bbc5345663e1
derrick steve kembou nganmoe <kembou.derrick@gmail.com>
2022-04-17 08:10:27 +0100

    creation des tableaux mines,freres,drapeau,decouver et des  foncitons dansX et dansY pour verifier a chaque clique sil sagit dune case

--- src/esp_demineur/Gui.java
+++ src/esp_demineur/Gui.java
@@ -13,12 +13,40 @@ public class Gui extends JFrame{
 	int espace=15;
 	int mx=-100;
 	int my=-100;
+	 Random rand=new Random();
+	int[][] mines=new int[16][9];
+	int[][] freres=new int [16][19];
+	boolean[][] reveler=new boolean[16][9];
+	boolean[][] drapeau=new boolean[16][9];
+	
+	
+	
+	
 	public Gui() {
 		this.setTitle("ESP DEMINEUR");
 		//donner une taille a lecran
 		this.setSize(1286,830);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
+		
+		for(int i=0; i<16; i++) {
+			for (int j=0; j<9; j++) {
+				if(rand.nextInt(100)<20) {
+					mines[i][j]=1;
+				}else {
+					mines[i][j]=0;
+				}
+				
+			}
+		}
+		
+		
+		
+		
+		
+		
+		
+		
 		Board board=new Board();
 		this.setContentPane(board);
 		Move move=new Move();
@@ -38,6 +66,9 @@ public class Gui extends JFrame{
 			for(int i=0; i<16; i++) {
 				for (int j=0; j<9; j++) {
 					g.setColor(Color.gray);
+				/*	if(mines[i][j]==1) {
+						g.setColor(Color.yellow);
+					}*/
 					if(mx>=espace+i*60&& mx<espace+i*60+80-2*espace &&my>=espace+j*80+26 && my< espace+j*80+26+80-2*espace) {
 						g.setColor(Color.red); 
 					}
@@ -70,7 +101,9 @@ public class Gui extends JFrame{
 		//lorsquon fera un clique de souris
 		@Override
 		public void mouseClicked(java.awt.event.MouseEvent e) {
-			System.out.print("je suis cliquer\n");
+			if(dansX()!=-1 && dansY()!=-1) {
+				System.out.print(mines[dansX()][dansY()]+"bon\n");
+			}else System.out.print("mauvais\n");
 			
 		}
 		//lorsquon va presser
@@ -99,6 +132,29 @@ public class Gui extends JFrame{
 			
 		}
 		
+		
+	}
+	public int dansX() {
+		for(int i=0; i<16; i++) {
+			for (int j=0; j<9; j++) {
+				if(mx>=espace+i*60&& mx<espace+i*60+80-2*espace &&my>=espace+j*80+26 && my< espace+j*80+26+80-2*espace) {
+					return i;  
+				}
+				
+				
+			}
+		}
+		return -1;
+		
+	}
+	public int dansY() {
+		for(int i=0; i<16; i++) {
+			for (int j=0; j<9; j++) {
+				if(mx>=espace+i*60&& mx<espace+i*60+80-2*espace &&my>=espace+j*80+26 && my< espace+j*80+26+80-2*espace) {
+					return j;
+				}	
+			}
+		}return -1;
 		
 	}
 
