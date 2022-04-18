8b29a48be97064052b85678ecc3557e1ba350b7b
derrick steve kembou nganmoe <kembou.derrick@gmail.com>
2022-04-18 17:10:13 +0100

    affichage du nombre de frere miné

--- src/esp_demineur/Gui.java
+++ src/esp_demineur/Gui.java
@@ -13,8 +13,10 @@ public class Gui extends JFrame{
 	int espace=15;
 	int mx=-100;
 	int my=-100;
+	
 	int freres=0;
 	 Random rand=new Random();
+	 
 	int[][] mines=new int[16][9];
 	int[][] voisins=new int [16][19];
 	boolean[][] reveler=new boolean[16][9];
@@ -24,7 +26,7 @@ public class Gui extends JFrame{
 	
 	
 	public Gui() {
-		this.setTitle("ESP DEMINEUR");
+		this.setTitle("MARIAME");
 		//donner une taille a lecran
 		this.setSize(1286,830);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,7 +34,7 @@ public class Gui extends JFrame{
 		
 		for(int i=0; i<16; i++) {
 			for (int j=0; j<9; j++) {
-				if(rand.nextInt(100)<20) {
+				if(   rand.nextInt(100)     <20) {
 					mines[i][j]=1;
 					
 				}else {
@@ -54,11 +56,9 @@ public class Gui extends JFrame{
 						
 						
 					}
-				}
-				//faire -1 si le clique a ete fait sur une mine
-				if(mines[i][j] ==1) voisins[i][j]=freres-1; else  voisins[i][j]=freres ;
-				
-				
+				}//faire -1 si le clique a ete fait sur une mine
+				if(mines[i][j]==1) voisins[i][j]=freres-1; else voisins[i][j]=freres;
+						
 			}
 		}
 		
@@ -70,9 +70,11 @@ public class Gui extends JFrame{
 		
 		
 		Board board=new Board();
+		
 		this.setContentPane(board);
 		Move move=new Move();
 		this.addMouseMotionListener(move);  
+		
 		Click click=new Click();
 		this.addMouseListener(click);
 		
@@ -81,7 +83,7 @@ public class Gui extends JFrame{
 	public class Board extends JPanel{
 		//fonction pour créer une composant
 		public void paintComponent(Graphics g) {
-			g.setColor(Color.DARK_GRAY);
+			g.setColor(Color.white);
 			//fonction pour créer un rectangle
 			g.fillRect(0,0,1280,800);
 			
@@ -90,7 +92,7 @@ public class Gui extends JFrame{
 					g.setColor(Color.gray);
 					//verifier si la case es reveler
 					if(reveler[i][j]==true) {
-						g.setColor(Color.white);
+						g.setColor(Color.blue);
 					}
 					if(mines[i][j]==1) {
 						g.setColor(Color.yellow);
@@ -99,6 +101,13 @@ public class Gui extends JFrame{
 						g.setColor(Color.LIGHT_GRAY); 
 					}
 					g.fillRect( espace+i*60, espace+j*80, 80-2*espace,80-2*espace);
+					if(reveler[i][j]==true) {
+						g.setColor(Color.white);
+						g.setFont(new Font("SansSerif",Font.BOLD,40));
+						g.drawString(Integer.toString(voisins[i][j]), i*60+27, j*80+55);
+					}
+					
+					
 					
 				}
 			}	
@@ -110,7 +119,7 @@ public class Gui extends JFrame{
 
 		@Override
 		public void mouseDragged(java.awt.event.MouseEvent e) {
-			// TODO Auto-generated method stub
+			// TODO Auto-generated method stub 
 			
 		}
 
@@ -118,7 +127,7 @@ public class Gui extends JFrame{
 		public void mouseMoved(java.awt.event.MouseEvent e) {
 			  mx=e.getX();
 			   my=e.getY();
-		//	System.out.print(mx+"le e e"+my);
+			//System.out.print("-----cordonnee en x ="+mx +"cordonnee en y="+my+"\n");
 			
 		}
 		
@@ -129,9 +138,10 @@ public class Gui extends JFrame{
 		@Override
 		public void mouseClicked(java.awt.event.MouseEvent e) {
 			//mettons reveler a true pour la case cliquer
+		//	System.out.print("-----------cliquer----------\n");
 			if(dansX()!=-1 && dansY()!=-1) {
 				reveler[dansX()][dansY()]=true;
-				System.out.print("--------le nombre de frère non miné est "+ voisins[dansX()][dansY()]+"----");
+				System.out.print("--------le nombre de frère non miné est "+ voisins[dansX()][dansY()]+"----\n");
 				
 			}
 			//a chaque fois que le user clique on appelle les fonction pour verifier si le clique a ete faite sur une case 
@@ -143,35 +153,35 @@ public class Gui extends JFrame{
 		//lorsquon va presser
 		@Override
 		public void mousePressed(java.awt.event.MouseEvent e) {
-			System.out.print("presser\n");
+		//	System.out.print("presser\n");
 			
 		}
 
 		@Override
 		public void mouseReleased(java.awt.event.MouseEvent e) {
-			System.out.print("relacher\n");
+			//System.out.print("relacher\n");
 			
 		}
 		//lorsquon entre dans la vue
 		@Override
 		public void mouseEntered(java.awt.event.MouseEvent e) {
-			System.out.print("Entrez\n");
+			//System.out.print("Entrez\n");
 			
 		}
 		//lorsquon ferme la vu
 
 		@Override
 		public void mouseExited(java.awt.event.MouseEvent e) {
-			System.out.print("Sortie\n");
+			//System.out.print("Sortie\n");
 			
-		}f
+		}
 		
 		
 	}
 	public int dansX() {
 		for(int i=0; i<16; i++) {
 			for (int j=0; j<9; j++) {
-				if(mx>=espace+i*60&& mx<espace+i*60+80-2*espace &&my>=espace+j*80+26 && my< espace+j*80+26+80-2*espace) {
+				if(    mx>=espace+i*60&& mx<espace+i*60+80-2*espace &&my>=espace+j*80+26 && my< espace+j*80+26+80-2*espace) {
 					return i;  
 				}
 				
